fe6c8734483aaea16a6cb37933b22ea0d21aeb70
hcheng1118 <hcheng1118@hotmail.com>
2021-05-31 09:01:53 -0500

    add drop-out option. The weibul distribution is assumed.

--- R/simu.fixed.R
+++ R/simu.fixed.R
@@ -14,27 +14,32 @@ simu.fixed <- function(bsl_dist=c("weibull","loglogistic")
                             # alpha=1 corresponds to exponential
                             ,totN
                             ,fHR #the non proportion hazard function
-                            ,prop #proportion of treatment group
+                            ,ratio # # of trt/# of placebo
                             ,entry =18 #entry time
                             ,fup=24
                             ,seed
                             ,upInt=100
+                            ,drop_param
 
 
 ){
 
-  # to note: n0 is the # of subjects in treatment group, which is assumed to
+  # to note: n1 is the # of subjects in treatment group, which is assumed to
   # have fewer events
-
-  n0 <- totN*prop
-  n1 <- totN-n0
-  trt <- c(rep(1,n0),rep(0,n1))
+  prop <- ratio/(ratio+1)
+  n1 <- ceiling(totN*prop)
+  n0 <- totN-n1
+  if (ceiling(n1)!=n1|ceiling(n0)!=n0){
+    stop("The number of subjects in each group must be interger. Check!")
+  }
+  trt <- c(rep(0,n0),rep(1,n1))
   a=param[1]
   b=param[2]
+
   if (bsl_dist=="weibull"){
     set.seed(seed)
     T_0 <- stats::rweibull(n0,a,1/b)
-    #-- get the cummulative hazard function----#
+    #-- get the cummulative hazard and survival function----#
     Hf <- function(t){exp(-1* a*b*stats::integrate( function(x){(x*b)^(a-1)*fHR(x)},0,t)$value)}
   }else if (bsl_dist=="loglogistic"){
     set.seed(seed)
@@ -49,16 +54,44 @@ simu.fixed <- function(bsl_dist=c("weibull","loglogistic")
 
   ## in extreme case, time is 0, add 0.1;
   Tm <- ifelse(Tm==0,0.1,Tm)
+  if (!missing(drop_param)){
+    a_drop <- drop_param[1]
+    b_drop <- drop_param[2]
+    set.seed(seed*99)
+    drop_time <- stats::rweibull(totN,a_drop,1/b_drop)
+  }else{
+    drop_time <- rep(1:length(totN),Inf)
+  }
 
   ## for entry time
   set.seed(seed+1)
   t0 <- runif(totN,0,entry)
   ot <- t0+Tm
+  Dur <- entry+fup # the length of study
+  dat <- data.frame(id=1:totN,ent=t0,time=Tm,trt=trt,ot=ot,
+                    drop_time=drop_time,ot_drop=t0+drop_time)
+  # find the smallest time
+  min_ind <- apply(cbind(dat$ot,Dur,dat$ot_drop),1,which.min)
+  status <- c("event","end of study","drop-out")
+  tot_len <- cbind(dat$ot,Dur,dat$ot_drop)[cbind(seq_along(min_ind),min_ind)]
+  dat$t_val <- tot_len-t0
+  dat$cnsr_desc <- status[min_ind]
+  dat$cnsr <- 1-(min_ind==1) #cnsr=1 indicates censoring
 
-  dat <- data.frame(id=1:totN,ent=t0,time=Tm,trt=trt,ot=ot)
-  dat$t_cnsr <- with(dat,ifelse(ot>entry+fup,0,1))
-  dat$t_val <- with(dat,ifelse(ot>entry+fup,entry+fup-ent,Tm))
+  final <- with (dat,data.frame(
+    id=id
+    ,group=trt
+    ,entry.time=ent
+    ,event.time=time
+    ,drop.time=drop_time
+    ,obs.time=tot_len
+    ,aval=t_val
+    ,cnsr=cnsr
+    ,cnsr.desc=cnsr_desc
+     ,event=1-cnsr
+  )
+  )
+#table(final$group,final$cnsr.desc)
 
-
-  return(dat)
+  return(final)
 }
