ee124d7f92efd00eac5d591ce53eeba78df5f2fe
hcheng1118 <hcheng1118@hotmail.com>
2021-07-12 20:54:36 -0400

    Initial commit

--- R/oxy-pwr2n.maxLR.R
+++ R/oxy-pwr2n.maxLR.R
@@ -26,12 +26,12 @@
 #'  }
 #' }
 #' @seealso 
-#'  \code{\link[stats]{stepfun}}
+#'  \code{\link[stats]{stepfun}},\code{\link[stats]{weighted.mean}}
 #'  \code{\link[mvtnorm]{qmvnorm}},\code{\link[mvtnorm]{pmvnorm}}
 #'  \code{\link[graphics]{par}},\code{\link[graphics]{lines}},\code{\link[graphics]{legend}}
 #' @rdname pwr2n.maxLR
 #' @export 
-#' @importFrom stats stepfun
+#' @importFrom stats stepfun weighted.mean
 #' @importFrom mvtnorm qmvnorm pmvnorm
 #' @importFrom graphics par lines legend
 pwr2n.maxLR<- function(entry   = 1
@@ -51,11 +51,14 @@ pwr2n.maxLR<- function(entry   = 1
   tot_time <- entry+fup
   num <- k*tot_time
 
-  x <- seq(0.01,tot_time,length=k)
+  #x <- seq(0.01,tot_time,length=k)
+  x <- seq(0,tot_time,by=1/k)
   haz_val <- hazR(x)*trans.prob[5]
   haz_point <- x*k
-  hazard <- stats::stepfun(haz_point,c(haz_val,haz_val[length(haz_val)]),
-                    right=TRUE)
+ # hazard <- stats::stepfun(haz_point,c(haz_val,haz_val[length(haz_val)]),
+  #                  right=TRUE)
+  hazard <- stats::stepfun(haz_point,c(haz_val[1],haz_val),
+                           right=FALSE)
   ##initial states for treatment
   D1 <- c(0,0,1,0,0)
   ##initial states for control
@@ -138,7 +141,7 @@ pwr2n.maxLR<- function(entry   = 1
     pdat[i,21] <- prod(1-pdat[1:i,14])
     ## mean survival
     # pdat[i,19] <- mean(sum(pdat[i,5],pdat[i,4]),sum(pdat[i,10],pdat[i,9]))
-    pdat[i,19] <- mean(c(pdat[i,20],pdat[i,21]))
+    pdat[i,19] <- stats::weighted.mean(c(pdat[i,20],pdat[i,21]),w=c(ratio,1))
     ## rho
     if (i==1){
       pdat[i,18] <- (pdat[i,3]+pdat[i,8])/rho
